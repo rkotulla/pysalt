19f00a2505d7716102de04fe167933deaba92145
Ralf Kotulla <ralf.kotulla@gmail.com>
2015-04-06 09:17:26 -0500

    Added new function get_binning to conveniently find out about the binning used in the given frame. Input can either be a filename, HDUList, pyfits.Header, or ImageHDU/PrimaryHDU.

--- pysalt/tools.py
+++ pysalt/tools.py
@@ -1,6 +1,7 @@
 
 import pysalt
 import os, sys
+import pyfits
 
 def get_data_filename(filepart):
 
@@ -22,3 +23,35 @@ def clobberfile(filename):
     if (os.path.isfile(filename)):
         os.remove(filename)
     return
+
+
+def get_binning(file_or_hdu):
+    
+    ccdsum = None
+
+    if (type(file_or_hdu) == pyfits.hdu.hdulist.HDUList):
+        if ('CCDSUM' in file_or_hdu[0].header):
+            ccdsum = file_or_hdu[0].header['CCDSUM']
+
+    elif (type(file_or_hdu) in (pyfits.hdu.image.PrimaryHDU,
+                                pyfits.hdu.image.ImageHDU)):
+        if ('CCDSUM' in file_or_hdu.header):
+            ccdsum = file_or_hdu.header['CCDSUM']
+
+    elif (type(file_or_hdu) == pyfits.header.Header):
+        if ('CCDSUM' in file_or_hdu):
+            ccdsum = file_or_hdu['CCDSUM']
+
+    elif (type(file_or_hdu) == str):
+        hdu = pyfits.open(file_or_hdu)
+        if ('CCDSUM' in hdu[0].header):
+            ccdsum = hdu[0].header['CCDSUM']
+
+    if (not ccdsum == None):
+        parts = ccdsum.split()
+        binx = int(parts[0])
+        biny = int(parts[1])
+        return binx, biny
+
+    return None, None
+            
